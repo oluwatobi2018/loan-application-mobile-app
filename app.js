94cdfb10472e881a9f8dd5b4437ad38c61b687ed
Christopher Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2022-10-19 08:05:32 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+import { createDrawerNavigator } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import ApplyScreen from './screens/ApplyScreen';
+import BvnScreen from './screens/BvnScreen';
+import HistoryScreen from './screens/HistoryScreen';
+import ChargeScreen from './screens/ChargeScreen';
+import AccountScreen from './screens/AccountScreen';
+
+"Image",https;//www.figma.com/file/ZKILoCoIoy1IESdBpq3GNC/Frontend
+
+
+const App = createDrawerNavigator({
+  HomeScreen: {screen: HomeScreen},
+  ApplyScreen: {screen: ApplyScreen},
+  BvnScreen: {screen: BvnScreen},
+  HistoryScreen: {screen: HistoryScreen},
+  ChargeScreen: {screen: ChargeScreen},
+  AccountScreen: {screen: AccountScreen}
+});
+
+export default App;
